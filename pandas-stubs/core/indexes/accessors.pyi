a691aa4ece92cc8ee72405973d6eee606789c296
lolpack <pollackaaron@gmail.com>
2025-06-11 11:48:36 -0400

    Create a complicated config to compare against pyright

--- pandas-stubs/core/indexes/accessors.pyi
+++ pandas-stubs/core/indexes/accessors.pyi
@@ -205,7 +205,7 @@ _DTToPeriodReturnType = TypeVar(
     "_DTToPeriodReturnType", bound=PeriodSeries | PeriodIndex
 )
 
-class _DatetimeLikeNoTZMethods( #pyrefly: ignore
+class _DatetimeLikeNoTZMethods(
     _DatetimeRoundingMethods[_DTTimestampTimedeltaReturnType],
     Generic[
         _DTTimestampTimedeltaReturnType,
@@ -237,7 +237,7 @@ class _DatetimeNoTZProperties(
         _DTOtherOpsTimeReturnType,
         _DTFreqReturnType,
     ],
-    _DatetimeLikeNoTZMethods[ # pyrefly: ignore
+    _DatetimeLikeNoTZMethods[
         _DTTimestampTimedeltaReturnType,
         _DTNormalizeReturnType,
         _DTStrKindReturnType,
